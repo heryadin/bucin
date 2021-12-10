4c9d66e436d401c04bbf57f15fbb6eca418af915
hariyadin <79754073+hariyadin404@users.noreply.github.com>
2021-12-10 13:28:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html><link type="text/css" rel="stylesheet" id="dark-mode-custom-link"><link type="text/css" rel="stylesheet" id="dark-mode-general-link"><style lang="en" type="text/css" id="dark-mode-custom-style"></style><style lang="en" type="text/css" id="dark-mode-native-style"></style><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>HARIYADIN</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/media/hariyadin.jpg" type="image/jpg">
+</head>
+
+<body data-new-gr-c-s-check-loaded="14.1014.0" data-gr-ext-installed="">
+    <img id="userPhoto" src="/media/hariyadin.jpg" alt="User Photo">
+    
+    <a href="https://www.instagram.com/hariyadin_official/" id="userName">@hariyadin_official</a>
+    <div id="links">
+        <a class="link" href="./bumcin/bucin.html">Bucin Web V 1</a>
+    </div>
+    <div id="links">
+        <a class="link" href="./bumcin/bucin2.html">Bucin Web V 2</a>
+    </div>
+    <div id="links">
+        <a class="link" href="./bumcin/bucin3.html">Bucin Web V 3</a>
+    </div>
+    <div id="links">
+        <a class="link" href="./bumcin/bucin4.html">Bucin Web V 4</a>
+    </div>
+        <div id="links">
+        <a class="link" href="./bumcin/bucin5.html">Bucin Web V 5</a>
+    </div>
+    <div id="links">
+        <a class="link" href="./creator/phlogo.html">Maker Generator</a>
+    </div>
+
+    <!-- Javascript -->
+    <script src="./script/jquery.min.js.download"></script>
+    <script src="./script/index.js.download"></script>
+
+</body></html>
